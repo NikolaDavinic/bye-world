a76ed1185ae987ccba5c955738144569a5be1c3e
stefan-foo <stojadinovicstefan59@gmail.com>
2022-12-26 17:04:19 +0100

    redis primer

---
+++ Bye World/ByeWorld-backend/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StackExchange.Redis;
+
+namespace ByeWorld_backend.Controllers
+{
+    [ApiController]
+    [Route("user")]
+    public class UserController : ControllerBase
+    {
+        private readonly IConnectionMultiplexer _redis;
+        public UserController(IConnectionMultiplexer redis)
+        {
+            _redis = redis;
+        }
+
+        [HttpGet("login")]
+        public async Task<IActionResult> Login()
+        {
+            var db = _redis.GetDatabase();
+            await db.StringSetAsync("user", "stefan");
+
+            return Ok((string)(await db.StringGetAsync("user")));
+        }
+    }
+}
--- Bye World/ByeWorld-backend/Program.cs
+++ Bye World/ByeWorld-backend/Program.cs
@@ -1,11 +1,14 @@
+using StackExchange.Redis;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IConnectionMultiplexer>(
+    ConnectionMultiplexer.Connect(builder.Configuration.GetConnectionString("redis")));
 
 var app = builder.Build();
 
