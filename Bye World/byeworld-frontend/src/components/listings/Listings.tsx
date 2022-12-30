f42a076b906e55e388738fc7f5f42e0e9a7dcde3
Milan Stojkovic <milanstojkovic2707@gmail.com>
2022-12-31 00:47:47 +0100

    Added state to Listings page

--- Bye World/byeworld-frontend/src/components/listings/Listings.tsx
+++ Bye World/byeworld-frontend/src/components/listings/Listings.tsx
@@ -1,15 +1,23 @@
 import { Button, Icon, TextField } from '@mui/material'
-import React from 'react'
+import React, { useState } from 'react'
 
 export const Listings: React.FC = () => {
+    const [keyword, setKeyword] = useState<string>("");
+    const [skill, setSkill] = useState<string>("");
+    const [city, setCity] = useState<string>("");
+    const [seniority, setSeniority] = useState<string>("");
+
     return (
-        <div className='h-1/4 flex flex-col px-4 py-4 space-y-4'>
-            <h2 className='font-bold'>Pretraga poslova</h2>
+        <div className='h-1/4 flex flex-col px-4 py-4 space-y-4 bg-gray-100'>
+            <h1 className='font-bold'>Listing search</h1>
             <div className='flex flex-row justify-center gap-6'>
-                <TextField id="keyword" label="Keyword search" variant="outlined" />
-                <TextField id="skill" label="Skill" variant="outlined" />
-                <TextField id="city" label="City" variant="outlined" />
-                <TextField id="seniority" label="Seniority" variant="outlined" />
+                <TextField id="keyword" label="Keyword search" variant="outlined" onChange={e => setKeyword(e.target.value)} value={keyword} />
+                <TextField id="skill" label="Skill" variant="outlined" onChange={e => setSkill(e.target.value)}
+                    value={skill} />
+                <TextField id="city" label="City" variant="outlined" onChange={e => setCity(e.target.value)}
+                    value={city} />
+                <TextField id="seniority" label="Seniority" variant="outlined" onChange={e => setSeniority(e.target.value)}
+                    value={seniority} />
                 <Button variant='contained' startIcon={
                     <Icon
                         sx={{ display: { xs: "none", md: "flex" }, mr: 1 }}
