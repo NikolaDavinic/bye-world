24da301fc18d8c70078bf47d96d2bc95f4c9929c
stefan-foo <stojadinovicstefan59@gmail.com>
2023-01-05 18:22:04 +0100

    user context

--- Bye World/byeworld-frontend/src/App.tsx
+++ Bye World/byeworld-frontend/src/App.tsx
@@ -3,7 +3,6 @@ import { ThemeProvider, createTheme } from "@mui/material";
 import { Route, Routes } from "react-router";
 import { SignUp } from "./components/SignUp/Signup";
 import { SignIn } from "./components/SignIn/Signin";
-import { UserProvider } from "./contexts/user.context";
 import { Listings } from "./components/listings/ListingsPage";
 import { Navbar } from "./components/common/Navbar/Navbar";
 import { Home } from "./components/Home/Home";
@@ -12,7 +11,8 @@ import Companies from "./components/CompaniesPage/CompaniesPage";
 import NotFound from "./components/NotFound/NotFound";
 import AddCompanyPage from "./components/AddCompanyPage/AddCompanyPage";
 import CompanyPage from "./components/CompanyPage/CompanyPage";
-import Footer from "./components/common/Footer/Footer";
+import { useAuthContext } from "./contexts/auth.context";
+import { useEffect } from "react";
 
 const theme = createTheme({
   palette: {
@@ -22,35 +22,40 @@ const theme = createTheme({
       dark: "#000063",
     },
     secondary: {
-      light: "#fbc02d",
+      light: "#ffd54f",
       main: "#ffd54f",
-      dark: "#000063",
+      dark: "#c29fob",
     },
   },
 });
 
 function App() {
+  const { autoLogin } = useAuthContext();
+
+  useEffect(() => {
+    autoLogin();
+  }, []);
+
   return (
     <ThemeProvider theme={theme}>
-      <UserProvider>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<Navbar />}>
-              <Route path="" element={<Home />}></Route>
-              <Route path="/home" element={<Home />}></Route>
-              <Route path="/listings" element={<Listings />}></Route>
-              <Route path="/user/:id" element={<User />}></Route>
-              <Route path="/companies" element={<Companies />}></Route>
-              <Route path="/addcompany" element={<AddCompanyPage />}></Route>
-              <Route path="/company/:id" element={<CompanyPage />}></Route>
-              <Route path="*" element={<NotFound />}></Route>
-              <Route path="/footer" element={<Footer />}></Route>
-            </Route>
-            <Route path="/signin" element={<SignIn />}></Route>
-            <Route path="/signup" element={<SignUp />}></Route>
-          </Routes>
-        </div>
-      </UserProvider>
+      {/* <AuthStateProvider> */}
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Navbar />}>
+            <Route path="" element={<Home />}></Route>
+            <Route path="/home" element={<Home />}></Route>
+            <Route path="/listings" element={<Listings />}></Route>
+            <Route path="/user/:id" element={<User />}></Route>
+            <Route path="/companies" element={<Companies />}></Route>
+            <Route path="/addcompany" element={<AddCompanyPage />}></Route>
+            <Route path="/company/:id" element={<CompanyPage />}></Route>
+            <Route path="*" element={<NotFound />}></Route>
+          </Route>
+          <Route path="/signin" element={<SignIn />}></Route>
+          <Route path="/signup" element={<SignUp />}></Route>
+        </Routes>
+      </div>
+      {/* </AuthStateProvider> */}
     </ThemeProvider>
   );
 }
--- Bye World/byeworld-frontend/src/index.tsx
+++ Bye World/byeworld-frontend/src/index.tsx
@@ -4,6 +4,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
+import { AuthStateProvider } from "./contexts/auth.context";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
@@ -12,7 +13,9 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthStateProvider>
+        <App />
+      </AuthStateProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
