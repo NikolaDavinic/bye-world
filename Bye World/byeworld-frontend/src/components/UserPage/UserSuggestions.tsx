352d98f3e9fc090d8271e6ca2e78285046e49fac
stefan-foo <stojadinovicstefan59@gmail.com>
2023-01-22 06:36:55 +0100

    suggested for you natpis

--- Bye World/byeworld-frontend/src/components/UserPage/UserSuggestions.tsx
+++ Bye World/byeworld-frontend/src/components/UserPage/UserSuggestions.tsx
@@ -36,8 +36,15 @@ const UserSuggestions = () => {
   }
 
   return (
-    <Box className="flex center py-5 justify-center">
+    <Box className="flex center py-3 justify-center">
       <Box className="xl:w-3/5 w-full">
+        <Typography
+          textAlign="left"
+          className="opacity-60 font-bold"
+          variant="h6"
+        >
+          Suggested for you
+        </Typography>
         <Grid container columns={{ xs: 1, md: 3 }}>
           {suggestions.map((item) => {
             return (
